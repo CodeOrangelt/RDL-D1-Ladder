7483f2a28a93c9efc70484baad2cc53ffe37f1ab
Malachi Walker <malachib1@hotmail.com>
2025-02-14 19:09:57 +0900

    updated FireBase connection keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDMF-bq4tpLoZvUYep_G-igmHbK2h-e-Zs",
+    authDomain: "RDLadder.firebaseapp.com",
+    projectId: "rdladder",
+    storageBucket: "RDladder.appspot.com",
+    messagingSenderId: "152922774046",
+    appId: "1:152922774046:web:c14bd25f07ad1aa0366c0f"
 };
 
 // Initialize Firebase
