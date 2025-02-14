fc69eee24b3f2a736d3c083b203b7c515d2ae8b2
Malachi Walker <malachib1@hotmail.com>
2025-02-14 18:30:51 +0900

    Secont Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Competitive Ladder</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Competitive Ladder</h1>
+    <?php include 'ladder.php'; ?>
+</body>
+</html>
