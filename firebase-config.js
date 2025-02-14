04f900ff749f275d7786fd2960a6e1a8d3042a37
Malachi Walker <malachib1@hotmail.com>
2025-02-14 19:12:10 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-// firebase-config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDMF-bq4tpLoZvUYep_G-igmHbK2h-e-Zs",
-    authDomain: "RDLadder.firebaseapp.com",
+    authDomain: "rdladder.firebaseapp.com",
     projectId: "rdladder",
-    storageBucket: "RDladder.appspot.com",
+    storageBucket: "rdladder.firebasestorage.app",
     messagingSenderId: "152922774046",
-    appId: "1:152922774046:web:c14bd25f07ad1aa0366c0f"
-};
+    appId: "1:152922774046:web:c14bd25f07ad1aa0366c0f",
+    measurementId: "G-MXVPNC0TVJ"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
