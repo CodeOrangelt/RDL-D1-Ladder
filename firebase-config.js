71898b7bd46cc28aae918149374590d534944b35
Malachi Walker <malachib1@hotmail.com>
2025-02-15 16:43:30 +0900

    we ball

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,19 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDMF-bq4tpLoZvUYep_G-igmHbK2h-e-Zs",
-    authDomain: "rdladder.firebaseapp.com",
-    projectId: "rdladder",
-    storageBucket: "rdladder.firebasestorage.app",
-    messagingSenderId: "152922774046",
-    appId: "1:152922774046:web:c14bd25f07ad1aa0366c0f",
-    measurementId: "G-MXVPNC0TVJ"
-  };
+  apiKey: "AIzaSyDMF-bq4tpLoZvUYep_G-igmHbK2h-e-Zs",
+  authDomain: "rdladder.firebaseapp.com",
+  projectId: "rdladder",
+  storageBucket: "rdladder.firebasestorage.app",
+  messagingSenderId: "152922774046",
+  appId: "1:152922774046:web:c14bd25f07ad1aa0366c0f",
+  measurementId: "G-MXVPNC0TVJ"
+};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 const db = firebase.firestore();
 const auth = firebase.auth();
