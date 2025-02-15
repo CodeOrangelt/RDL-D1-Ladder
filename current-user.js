34a6c067378c17b7c16b477a2ce15573c5134332
Malachi Walker <malachib1@hotmail.com>
2025-02-15 20:41:48 +0900

    Update current-user.js

--- current-user.js
+++ current-user.js
@@ -8,16 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (user) {
             // User is signed in.
-            currentUserSpan.textContent = user.displayName || user.email;
             currentUserSpan.style.display = 'inline'; // Show the username
-            signOutLink.style.display = 'inline'; // Make sure sign-out link is visible
+            signOutLink.style.display = 'block'; // Show the sign-out link
             loginRegisterLink.style.display = 'none'; // Hide login/register
         } else {
             // No user is signed in.
-            currentUserSpan.textContent = ''; // Clear the username
             currentUserSpan.style.display = 'none'; // Hide the username
             signOutLink.style.display = 'none'; // Hide the sign-out link
-            loginRegisterLink.style.display = 'inline-block'; // Show login/register
+            loginRegisterLink.style.display = 'block'; // Show login/register
         }
     });
 });
