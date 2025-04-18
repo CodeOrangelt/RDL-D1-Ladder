7c1d7552422a4d4c75e5c187cd805326bf1772e9
CodeOrangelt <36940752+CodeOrangelt@users.noreply.github.com>
2025-04-18 10:15:47 +0000

    Deploying to gh-pages from @ CodeOrangelt/RDL-D1-Ladder@3eeef046cbe4426323d292831612b4a7a734d3a4 🚀

--- root-js/admin-check.js
+++
@@ -1,4 +0,0 @@
-export function isAdmin(email) {
-    const ADMIN_EMAILS = ['admin@ladder.com', 'brian2af@outlook.com'];
-    return email && ADMIN_EMAILS.includes(email.toLowerCase());
-}
--- root-js/admin-config.js
+++
@@ -1,5 +0,0 @@
-// Update admin-config.js
-export const ADMIN_EMAILS = [
-    'admin@ladder.com',
-    'brian2af@outlook.com'  // Changed to lowercase
-];
